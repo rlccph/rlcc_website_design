2eb500f14e124101883227bd0da297524048a136
Raven Dee <raven.delacruz@rlcc.ph>
2026-06-16 11:35:01 +0800

    Add @vercel/speed-insights dependency and integrate SpeedInsights component into layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist_Mono, Montserrat } from "next/font/google";
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
@@ -37,7 +38,10 @@ export default function RootLayout({ children }) {
       lang="en"
       className={`${geistMono.variable} ${montserrat.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        {children}
+        <SpeedInsights />
+      </body>
     </html>
   );
 }
